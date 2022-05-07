fc52ef3ad138ab13e2ca054b0bd3c68acf69d6cf
Abhishek <abhishek.pandey.3237@gmail.com>
2022-05-07 12:05:08 +0530

    removed flask_wtf dependency in form

--- app/main/views.py
+++ app/main/views.py
@@ -1,24 +1,11 @@
 from flask import render_template, request, jsonify
 
 from . import main
-from .forms import CodeForm
 from .compilerapi import process
 
-@main.route('/', methods=['GET','POST'])
+@main.route('/', methods=['GET'])
 def home():
-    form = CodeForm()
-    output = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            language = form.language.data
-            script = form.script.data
-            output = process(language, script)
-            print(f"{language=}")
-            print(f"{script=}")
-            print(f"{output=}")
-            #print(f"{request.form['codescript']=}")
-            output = output['output'] if 'output' in output else output['error']
-    return render_template("index.html", form=form, output=output)
+    return render_template("index.html")
 
 @main.route('/api', methods=['GET','POST'])
 def api():
