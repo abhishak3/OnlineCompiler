04df7a80d0ce15e3c8ba888098cedcefb56a0f6e
abhishak3 <abhishek.pandey.3237@gmail.com>
2023-06-22 19:21:33 +0530

    ace editor update

--- app/static/js/ace_editor.js
+++ app/static/js/ace_editor.js
@@ -1,24 +1,25 @@
 // mappings from api-lang-name to ace-lang-name
 let languages = {
-    "python3": "python",
-    "java": "java",
-    "cpp": "c_cpp",
-    "cpp14": "c_cpp",
-    "cpp17": "c_cpp",
-    "csharp": "csharp",
-    "sql": "mysql",
-    "perl": "perl",
-    "go": "golang",
-    "bash": "batchfile"
+  "python3": "python",
+  "java": "java",
+  "cpp": "c_cpp",
+  "cpp14": "c_cpp",
+  "cpp17": "c_cpp",
+  "csharp": "csharp",
+  "sql": "mysql",
+  "perl": "perl",
+  "go": "golang",
+  "bash": "batchfile"
 }
 
 // Ace Editor Configs
 var editor = ace.edit("editor", {
-    autoScrollEditorIntoView: true,
-    minLines: 39,
-    maxLines: 39,
-    fontSize: 14,
-    cursorStyle: "smooth"
+  highlightActiveLine: true,
+  autoScrollEditorIntoView: true,
+  minLines: 39,
+  maxLines: 39,
+  fontSize: 14,
+  cursorStyle: "smooth"
 });
 
 // intial configs
@@ -29,16 +30,16 @@ editor.setTheme(`ace/theme/${select_theme.value}`);
 
 // jquery
 $(function() {
-    // changing language mode of editor
-    $("#language").on("change", function() {
-        let lang = languages[$(this).val()];
-        editor.session.setMode(`ace/mode/${lang}`);
-    })
+  // changing language mode of editor
+  $("#language").on("change", function() {
+    let lang = languages[$(this).val()];
+    editor.session.setMode(`ace/mode/${lang}`);
+  })
 
-    // changing editor theme
-    $("#theme").on("change", function() {
-        let theme = $(this).val();
-        editor.setTheme(`ace/theme/${theme}`);
-    })
+  // changing editor theme
+  $("#theme").on("change", function() {
+    let theme = $(this).val();
+    editor.setTheme(`ace/theme/${theme}`);
+  })
 })
 
